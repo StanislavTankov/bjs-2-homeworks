0923e13af95315e720fcc895d559cccf08e71000
Stanislav Tankov <tankov.s.s@bk.ru>
2021-09-14 20:24:50 +0500

    homeWorks - Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,29 +1,51 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = arr[1];
+  let avg = 0;
+
+  for (let i=0; i < arr.lenght; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    }else if(arr[i]>max){
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+    avg = (sum / arr.lenght).toFixed(2);
+  }
 
   // Ваш код
 
-  return { min:min, max:max, avg:avg };
+  return { min:min, max:max, avg:Number(avg) };
 }
+
+getArrayParams(99,-12,11);
+
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i=0; i<arr.lenght; i++){
+    sum+=arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
-  return max
+  let max = -Infinity;
+  for ( let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    console.log(sum);
+    if ( sum > max) {
+      max = sum;
+    }
+  }
 }
+return max;
+return makeWork(arrOfArr, worker);
+
 
 // Задание 3
 function worker2(arr) {
