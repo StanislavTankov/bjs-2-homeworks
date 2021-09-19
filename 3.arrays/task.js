8386c88141998a2cc388ce52918e85f583086a5b
Stanislav Tankov <tankov.s.s@bk.ru>
2021-09-19 18:27:07 +0500

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = (arr1.length === arr2.length) && arr1.every ((Element, index) => Element === arr2[index]);
+  return result; 
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+compareArrays([2, 45, 33], [33, 45, 33]);
 
-  // Ваш код
+
+
+// задание 2
+function advancedFilter(arr) {
+  let resultArr = arr.filter((number) => number > 0) && arr.filter((number) => % 3 === 0).map ((number) => number * 10);
 
   return resultArr; // array
 }
+
+advancedFilter([-4, 5, 8, 55, 33,-25]);
