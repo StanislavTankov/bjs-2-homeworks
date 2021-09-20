332caa5061139082d60a05eb9cd8f4526e505a5e
Stanislav Tankov <tankov.s.s@bk.ru>
2021-09-20 19:49:30 +0500

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,5 @@
 function compareArrays(arr1, arr2) {
-  let result = (arr1.length === arr2.length) && arr1.every ((Element, index) => Element === arr2[index]);
-  return result; 
+  return (arr1.length === arr2.length) && arr1.every ((element, index) => element === arr2[index]); 
 }
 
 compareArrays([2, 45, 33], [33, 45, 33]);
@@ -9,9 +8,7 @@ compareArrays([2, 45, 33], [33, 45, 33]);
 
 // задание 2
 function advancedFilter(arr) {
-  let resultArr = arr.filter((number) => number > 0) && arr.filter((number) => % 3 === 0).map ((number) => number * 10);
-
-  return resultArr; // array
+  return arr.filter((number) => number > 0) && arr.filter((number) => % 3 === 1).map ((number) => number * 10);
 }
 
 advancedFilter([-4, 5, 8, 55, 33,-25]);
