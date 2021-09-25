53e57956f955c9b8a15aa45783ba6c32ddfaad69
Stanislav Tankov <tankov.s.s@bk.ru>
2021-09-25 19:55:17 +0500

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,31 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name=name;
+  this.gender=gender;
+  this.age=age;
 }
+  const studentOne = new Student("Иван", "Мужчина", 25);
+  const studentTwo = new Student("Олеся", "Женщина", 24);
+  const studentThree = new Student("Олег", "Мужчина", 27);
+
+//console.log(studentOne.gender);
+//console.log(studentTwo.name);
+//console.log(studentThree.age);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+studentOne.setSubject("Алгебра");
+studentTwo.setSubject("Информатика");
+studentThree.setSubject("Английский");
+
+//console.log(studentOne.subject);
+//console.log(studentTwo.subject);
+//console.log(studentThree.subject);
+
+Student.prototype.addMark(mark) = function (...marks){
+  if (this.marks === undefined){
+    
+  }
+}
+
